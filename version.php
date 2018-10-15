42455fbf09e4d8bc475f4cadb0fdd11b3cab5918
Luís Augusto Weber Mercado <luiswebmercado@gmail.com>
2018-10-15 15:57:37 -0300

    Adicionando versão

--- version.php
+++ version.php
@@ -4,4 +4,5 @@ defined('MOODLE_INTERNAL') || die;
 
 $plugin->version = 2017051500;    // The current plugin version (Date: YYYYMMDDXX)
 $plugin->requires = 2017050500;    // Requires this Moodle version
+$plugin->release   = '1.0.0';
 $plugin->component = 'report_tecmides';  // Full name of the plugin (used for diagnostics)
