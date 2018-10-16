7fef5280f7626aa25cdd564cf9501a20d578c360
Alan Velasques Santos <alanvelasques@gmail.com>
2018-10-16 12:25:31 -0300

    ajustes do travis no arquivo constants

--- constants.php
+++ constants.php
@@ -1,4 +1,29 @@
 <?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Intermediator for managing actions executed by the Cognitiva Brasil server.
+ *
+ * @package   tecmides
+ * @copyright 2018 - Cognitiva Brasil
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v2 or later
+ * @author    Luis Mercado (lawmercado [at] inf [dt] ufrgs [dt] br)
+ */
+
+require_once(dirname(dirname(dirname(__FILE__))).'/config.php');
 
 global $CFG;
 
